e8867b2a3af269d5441dcf44f1f0fe6b805b5f33
TMY <TMY@DESKTOP-7S7GL73>
2025-02-21 03:06:29 +0000

    Add project files.

---
+++ APIToken/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using APIToken.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIToken.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+        public class ProductController : ControllerBase
+        {
+            private readonly ProductContext _context;
+            public ProductController(ProductContext context)
+            {
+                   _context = context;
+            }
+            
+        [HttpGet("Id")]
+
+        public async Task<IActionResult>GetById(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            return product == null ? NotFound() : Ok(product);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            return await _context.Products.ToListAsync();
+        }
+          
+        }
+    }
+
